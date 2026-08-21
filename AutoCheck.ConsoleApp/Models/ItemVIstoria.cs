1c6d76bc75664d2ad3c3cc31333b07e75ec9e812
VitorPeruch <peruchevitor@email.com>
2026-08-21 20:37:45 -0300

    criação do itemvistoria

---
+++ AutoCheck.ConsoleApp/Models/ItemVIstoria.cs
@@ -0,0 +1,16 @@
+
+namespace Vistoria
+{
+    public class ItemVistoria
+    {
+        public string Nome { get; set;}
+        public string Status { get; set;}
+        public ItemVistoria(string Nome,string Status)
+        {
+            if(Status == "Bom" || Status == "Regular" || Status == "Ruim")
+            {
+                this.Status = Status;
+            }
+        }
+    }
+}
